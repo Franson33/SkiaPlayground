1c48bf19df169e62117845958cdde3691f45c718
Franson33 <antonchikfr@gmail.com>
2023-06-12 21:24:14 +0300

    implement first simple animation on translateY gesture

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {AppNavigator} from './src/index';
+import {StyleSheet} from 'react-native';
 
 export const App: FC = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <GestureHandlerRootView style={styles.gestureRootView}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  gestureRootView: {
+    flex: 1,
+  },
+});
--- src/One.tsx
+++ src/One.tsx
@@ -1,25 +1,66 @@
 import React, {FC} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {
+  GestureDetector,
+  Gesture,
+  PanGestureHandlerEventPayload,
+  GestureUpdateEvent,
+} from 'react-native-gesture-handler';
+import {
+  useSharedValue,
+  useDerivedValue,
+  interpolate,
+  Extrapolation,
+} from 'react-native-reanimated';
 import {Canvas, Circle, Group} from '@shopify/react-native-skia';
-import {px} from './index';
+import {height, px} from './index';
 
 export const One: FC = () => {
-  const size = px(256);
-  const r = size * 0.33;
+  const offsetY = useSharedValue(0);
+  const startY = useSharedValue(0);
+  const size = useSharedValue(px(256));
+
+  const r = useDerivedValue(() => size.value * 0.33);
+  const sizeMinusR = useDerivedValue(() => size.value - r.value);
+  const sizeDivideTwo = useDerivedValue(() => size.value / 2);
+
+  const gesture = Gesture.Pan()
+    .onUpdate((e: GestureUpdateEvent<PanGestureHandlerEventPayload>) => {
+      offsetY.value = e.translationY + startY.value;
+
+      const newSize = interpolate(
+        offsetY.value,
+        [0, height],
+        [256, 512],
+        Extrapolation.CLAMP,
+      );
+
+      size.value = newSize;
+    })
+    .onEnd(() => {
+      startY.value = offsetY.value;
+    });
 
   return (
-    <Canvas style={styles.container}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={size - r} cy={r} r={r} color="magenta" />
-        <Circle cx={size / 2} cy={size - r} r={r} color={'yellow'} />
-      </Group>
-    </Canvas>
+    <GestureDetector gesture={gesture}>
+      <View style={styles.gestureWrapper}>
+        <Canvas style={styles.container}>
+          <Group blendMode="multiply">
+            <Circle cx={r} cy={r} r={r} color="cyan" />
+            <Circle cx={sizeMinusR} cy={r} r={r} color="magenta" />
+            <Circle cx={sizeDivideTwo} cy={sizeMinusR} r={r} color={'yellow'} />
+          </Group>
+        </Canvas>
+      </View>
+    </GestureDetector>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  gestureWrapper: {
     flex: 1,
   },
 });
